3363553145da06bd95537188552f2cfc4a8b9b5f
TheoZerbibi <tzeribi@gmail.com>
2018-11-27 16:34:25 +0100

    FixBug

--- Code/!Démarrer le Bingo.bat
+++ Code/!Démarrer le Bingo.bat
@@ -1,4 +1,4 @@
-iF EXIST node_modules (
+IF EXIST node_modules (
 	start http://localhost:3000/ && yarn start
 ) ELSE (
 	npm i -g yarn && npm i && yarn build && start http://localhost:3000/ && yarn start
