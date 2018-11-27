aca8b998d47d0c59ba7caabc68c88edf56c35fa4
TheoZerbibi <tzeribi@gmail.com>
2018-11-27 10:42:21 +0100

    Modif batch file

--- !Démarrer le Bingo.bat
+++ !Démarrer le Bingo.bat
@@ -1,1 +1,5 @@
-start http://localhost:3000/ && yarn start
+iF EXIST node_modules (
+	start http://localhost:3000/ && yarn start
+) ELSE (
+	npm i -g yarn && npm i && yarn build && start http://localhost:3000/ && yarn start
+)
