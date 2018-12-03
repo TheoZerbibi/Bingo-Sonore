e353399a21c6e6063f1b70f28b3be1f5b36b1613
TheoZerbibi <tzeribi@gmail.com>
2018-12-03 16:59:39 +0100

    ijfes

--- Code/src/main.js
+++ Code/src/main.js
@@ -20,6 +20,16 @@ $("#randomStart").click(function() {
 
 	var randomSong = Object.keys(list)[Math.floor(Math.random() * listSize)];
 	console.log(randomSong);
+	if (randomSong === 'poule') {
+		console.log('Poule');
+		console.log(list.poule.nbr);
+	} else if (randomSong === 'chassedeau') {
+		console.log('Chassedeau');
+		console.log(list.chassedeau.nbr);
+	} else if (randomSong === 'test') {
+		console.log('Test');
+		console.log(list.test.nbr);
+	}
 
 	/*var audio = new Audio(`./song/${randomSong}.mp3`);
 	audio.play();
