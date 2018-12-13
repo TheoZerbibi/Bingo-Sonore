443f0d61b1a2dba0023a1b214aac10626fb308f1
TheoZerbibi <tzeribi@gmail.com>
2018-12-13 14:00:04 +0100

    Modif .bat

--- Code/!Démarrer le Bingo.bat
+++ Code/!Démarrer le Bingo.bat
@@ -1,5 +1,6 @@
+git pull
 IF EXIST node_modules (
-	git pull && start http://localhost:3000/ && yarn start
+	start http://localhost:3000/ && yarn start
 ) ELSE (
 	npm i -g yarn && npm i && yarn build && start http://localhost:3000/ && yarn start
 )
